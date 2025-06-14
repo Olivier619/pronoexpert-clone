06d29bb859248316371fe271a5f56e782cd93e99
Olivier619 <132900963+Olivier619@users.noreply.github.com>
2025-06-14 20:14:00 +0200

    Add files via upload

---
+++ ChevronUpIcon.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+interface ChevronUpIconProps extends React.SVGProps<SVGSVGElement> {}
+
+export const ChevronUpIcon: React.FC<ChevronUpIconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    {...props}
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+  </svg>
+);
+    
